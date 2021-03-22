027c4a99064170dea712412c90132b95ccc648c2
andonilerchundi <andonivalero@gmail.com>
2021-03-22 15:57:41 +0100

    ejemplo CRUD maquetacion

---
+++ resources/views/admin/layout/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="">
+    
+    <title>Laravel</title>
+
+    @include('admin.layout.partials.style')
+</head>
+<body>
+    <div class="main"> 
+        @yield('content')
+    </div>
+</body>
+</html>

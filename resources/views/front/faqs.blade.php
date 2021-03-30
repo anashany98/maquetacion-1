c71caf589c7f22a573cda8a1e919cdfb59b91a89
andonilerchundi <andonivalero@gmail.com>
2021-03-30 23:49:58 +0200

    maquetacion faq

--- resources/views/front/faqs.blade.php
+++ resources/views/front/faqs.blade.php
@@ -2,18 +2,22 @@
 
 @section('content')
 
-<div class="main">
-    <div class="header">
-        <h1 class="titulo">FAQ</h1>
+<div class="header">
+    <div class="menu-button">
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+        </svg>
     </div>
-    <div class="faq">
-    cuerpo
-
-
-
-
-
-
-
-    </div>
+   <span>FAQ</span>
 </div>
+
+<div class="faq-content">
+    <div class="title">
+        Primera pregunta
+    </div>
+    <div class="description">
+        Primera respuesta
+    </div>
+    
+</div>
+
